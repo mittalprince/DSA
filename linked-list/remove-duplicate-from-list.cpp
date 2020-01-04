dc81ca322b615acdeca9b7ecf5bf017fd50fa581
mittalprince <princemittal766@gmail.com>
2020-01-04 12:08:40 +0530

    Modify Delete Duplicate Linked List Ans

--- linked-list/remove-duplicate-from-list.cpp
+++ linked-list/remove-duplicate-from-list.cpp
@@ -1,29 +1,44 @@
 ListNode* Solution::deleteDuplicates(ListNode* A) {
-    map<int, int>m;
-    ListNode* temp = A;
-    ListNode* prev = A;
-    while(temp != NULL){
-        if(m.find(temp->val) == m.end()){
-            m[temp->val]++;
-            prev = temp;
-            temp = temp->next;
-        }
-        else{
-            ListNode *t = temp;
-            if(temp->next){
-                prev->next = temp->next;
-                // prev = prev->next;
-                temp = temp->next;
-            }
-            else{
-                prev->next = NULL;
-                temp = NULL;
-            }
-            delete t;
-        }
-        // prev = temp;
-        // temp = temp->next;
+
+    if(A == NULL || A->next ==NULL){
+        return A;
     }
     
-    return A;
+    ListNode *curr=A, *start=A, *temp=NULL;
+    
+    while(curr){
+        temp = curr;
+        while(temp->next && temp->val == temp->next->val){
+            temp = temp->next;
+        }
+        
+        curr->next = temp->next;
+        curr = curr->next;
+    }
+    return start;
+    
+    // map<int, int>m;
+    // ListNode* temp = A;
+    // ListNode* prev = A;
+    // while(temp != NULL){
+    //     if(m.find(temp->val) == m.end()){
+    //         m[temp->val]++;
+    //         prev = temp;
+    //         temp = temp->next;
+    //     }
+    //     else{
+    //         ListNode *t = temp;
+    //         if(temp->next){
+    //             prev->next = temp->next;
+    //             temp = temp->next;
+    //         }
+    //         else{
+    //             prev->next = NULL;
+    //             temp = NULL;
+    //         }
+    //         delete t;
+    //     }
+    // }
+    
+    // return A;
 }
--- linked-list/remove-duplicate-from-list2.cpp
+++ linked-list/remove-duplicate-from-list2.cpp
@@ -6,29 +6,98 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 ListNode* Solution::deleteDuplicates(ListNode* A) {
-    map<int, int>m;
-    ListNode* temp = A, *head = NULL,  *tail = NULL;
     
-    while(temp != NULL){
-        m[temp->val]++;
+    if(A == NULL || A->next == NULL){
+        return A;
     }
     
-    for(auto it=m.begin(); it != m.end(); it++){
-        if(it->second == 1){
-            if(tail == NULL){
-                head = tail = new ListNode(it->first);
-            }
-            else{
-                tail->next = new ListNode(it->first);
-                tail = tail->next;
-            }
+    ListNode *start = new ListNode(0);
+    start->next = A;
+    ListNode *curr = start;
+    ListNode *temp;
+    
+    while(curr->next){
+        temp = curr->next;
+        while(temp->next && temp->val ==temp->next->val){
+            temp = temp->next;
+        }
+        
+        if(curr->next == temp){
+            curr = curr->next;
+        }
+        else{
+            curr->next = temp->next;
         }
     }
     
-    A = head;
-    return A;
+    return start->next;
+    // -------------------------------------------
+    
+    // if(A == NULL || A->next == NULL){
+    //     return A;
+    // }
+    
+    // ListNode *curr=A, *temp=A, *start=A, *prev=NULL;
+    
+    // while(curr){
+    //     while(curr->next && curr->val == curr->next->val){
+    //         curr = curr->next;
+    //     }
+        
+    //     if(curr != temp){
+    //         if(start == temp){
+    //             start = curr->next;
+    //             temp = curr->next;
+    //         }
+    //         else{
+    //             prev->next = curr->next;
+    //             temp = prev->next;
+    //         }
+    //     }
+    //     else{
+    //         prev = temp;
+    //         temp = curr->next;
+    //     }
+        
+    //     curr = curr->next;
+    // }
+    
+    // return start;
+    
+    
+    // ------------------------------------------------
+    // map<int, int>m;
+    // ListNode* temp = A, *head = NULL,  *tail = NULL;
+    
+    // while(temp != NULL){
+    //     m[temp->val]++;
+    // }
+    
+    // for(auto it=m.begin(); it != m.end(); it++){
+    //     if(it->second == 1){
+    //         if(tail == NULL){
+    //             head = tail = new ListNode(it->first);
+    //         }
+    //         else{
+    //             tail->next = new ListNode(it->first);
+    //             tail = tail->next;
+    //         }
+    //     }
+    // }
+    
+    // A = head;
+    // return A;
 }
+
 
 
 /*
